be52f245ff27fcb72b9641b65c4355a3beb6f452
fuutianyii <fuutianyii@gmail.com>
2022-12-17 14:20:23 +0800

    main

--- dict.php
+++ dict.php
@@ -9,12 +9,15 @@
     <title>dict</title>
 </head>
 <body>
+    <form name='date' action='books.php' method='post'>
+        <input type='hidden' name='data_validity_period' id= "data_validity_period" value=''/>
+    </form>
     <div id="main">
         <div id="left">
             <a href="#"><img src="img/menu.png" alt=""></a>
             <a href="#"><img src="img/user.jpg" alt=""><span>用户名</span></a>
             <a href="dict.php?page=search"><img src="img/search.png" alt=""><span>查词</span></a>
-            <a href="dict.php?page=books"><img src="img/test.png" alt=""><span>测试</span></a>
+            <a href='javascript:var myDate=new Date();var myDate=myDate.toLocaleDateString();var myDate=localStorage.data_validity_period;document.getElementById("data_validity_period").value=myDate;document.date.submit();'><img src="img/test.png" alt=""><span>测试</span></a>
             <a href="#"><img src="img/control.png" alt=""><span>偏好</span></a>
         </div>
         <?php
