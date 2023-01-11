505d85c541165b0e134649e7ee45c71175b8ba23
fuutianyii <fuutianyii@gmail.com>
2023-01-11 16:37:54 +0800

    main

--- books_info.php
+++ books_info.php
@@ -3,7 +3,7 @@
  * @Author: fuutianyii
  * @Date: 2022-11-01 17:56:03
  * @LastEditors: fuutianyii
- * @LastEditTime: 2023-01-10 17:57:21
+ * @LastEditTime: 2023-01-11 11:31:00
  * @github: https://github.com/fuutianyii
  * @mail: fuutianyii@gmail.com
  * @QQ: 1587873181
@@ -91,7 +91,8 @@ for($i=0; $i<$size; $i++)
             $review_array=[];
             
             $insert_date=date("Y-m-d",strtotime("-1 days"));
-            $mysqlselect="select word_id from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
+
+            $mysqlselect="select word_id,self_defining from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
             $mysqlselect=$pdo->prepare($mysqlselect);
             $mysqlselect->execute(array(':books_id'=>$books_id,":insert_date"=>$insert_date));
             $getall=$mysqlselect->fetchAll();
@@ -99,7 +100,7 @@ for($i=0; $i<$size; $i++)
 
 
             $insert_date=date("Y-m-d",strtotime("-2 days"));
-            $mysqlselect="select word_id from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
+            $mysqlselect="select word_id,self_defining from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
             $mysqlselect=$pdo->prepare($mysqlselect);
             $mysqlselect->execute(array(':books_id'=>$books_id,":insert_date"=>$insert_date));
             $getall=$mysqlselect->fetchAll();
@@ -107,7 +108,7 @@ for($i=0; $i<$size; $i++)
 
 
             $insert_date=date("Y-m-d",strtotime("-4 days"));
-            $mysqlselect="select word_id from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
+            $mysqlselect="select word_id,self_defining from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
             $mysqlselect=$pdo->prepare($mysqlselect);
             $mysqlselect->execute(array(':books_id'=>$books_id,":insert_date"=>$insert_date));
             $getall=$mysqlselect->fetchAll();
@@ -115,7 +116,7 @@ for($i=0; $i<$size; $i++)
 
 
             $insert_date=date("Y-m-d",strtotime("-7 days"));
-            $mysqlselect="select word_id from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
+            $mysqlselect="select word_id,self_defining from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
             $mysqlselect=$pdo->prepare($mysqlselect);
             $mysqlselect->execute(array(':books_id'=>$books_id,":insert_date"=>$insert_date));
             $getall=$mysqlselect->fetchAll();
@@ -123,7 +124,7 @@ for($i=0; $i<$size; $i++)
 
 
             $insert_date=date("Y-m-d",strtotime("-15 days"));
-            $mysqlselect="select word_id from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
+            $mysqlselect="select word_id,self_defining from word_family where (books_id=:books_id)  and  (insert_date=:insert_date)";
             $mysqlselect=$pdo->prepare($mysqlselect);
             $mysqlselect->execute(array(':books_id'=>$books_id,":insert_date"=>$insert_date));
             $getall=$mysqlselect->fetchAll();
@@ -135,24 +136,45 @@ for($i=0; $i<$size; $i++)
                 $word_size = count($review_array);    //取得数组单元个数
                 for($ws=0; $ws<$word_size; $ws++)
                 {
-                    $mysqlselect="select english,chinese,posd,US,UK from words where word_id=:word_id";
-                    $mysqlselect=$pdo->prepare($mysqlselect);
-                    $mysqlselect->execute(array(':word_id'=>$review_array[$ws][0]));
-                    $review_array[$ws]=NULL;
-                    $english=$mysqlselect->fetch();
-                    $posd=0;
-                    while ($english != NULL)
-                    {
-                        $tidy_english=[];
-                        for ($m=0;$m<count($english)/2;$m++)
-                        {
-                            array_push($tidy_english,$english[$m]);
-                        }
-        
-                        $review_array[$ws][$posd]=$tidy_english;
+                    if ($review_array[$ws][1] == "1"){
+                        $mysqlselect="select english,chinese,posd,US,UK from self_defining_means where word_id=:word_id";
+                        $mysqlselect=$pdo->prepare($mysqlselect);
+                        $mysqlselect->execute(array(':word_id'=>$review_array[$ws][0]));
+                        $review_array[$ws]=NULL;
                         $english=$mysqlselect->fetch();
-                        $posd++;
-                        
+                        $posd=0;
+                        while ($english != NULL)
+                        {
+                            $tidy_english=[];
+                            for ($m=0;$m<count($english)/2;$m++)
+                            {
+                                array_push($tidy_english,$english[$m]);
+                            }
+            
+                            $review_array[$ws][$posd]=$tidy_english;
+                            $english=$mysqlselect->fetch();
+                            $posd++;
+                        }
+                    }
+                    else{
+                        $mysqlselect="select english,chinese,posd,US,UK from words where word_id=:word_id";
+                        $mysqlselect=$pdo->prepare($mysqlselect);
+                        $mysqlselect->execute(array(':word_id'=>$review_array[$ws][0]));
+                        $review_array[$ws]=NULL;
+                        $english=$mysqlselect->fetch();
+                        $posd=0;
+                        while ($english != NULL)
+                        {
+                            $tidy_english=[];
+                            for ($m=0;$m<count($english)/2;$m++)
+                            {
+                                array_push($tidy_english,$english[$m]);
+                            }
+            
+                            $review_array[$ws][$posd]=$tidy_english;
+                            $english=$mysqlselect->fetch();
+                            $posd++;
+                        }
                     }
                 }
                 array_push($data[$groups[$i][1]],$review_array);
@@ -174,7 +196,7 @@ for($i=0; $i<$size; $i++)
         
             $data[$groups[$i][1]]=array($words_count);
         
-            $mysqlselect="select word_id from word_family where books_id=:books_id limit ".$progress.",50";
+            $mysqlselect="select word_id,self_defining from word_family where books_id=:books_id limit ".$progress.",50";
             // echo $mysqlselect;
             $mysqlselect=$pdo->prepare($mysqlselect);
             $mysqlselect->execute(array(':books_id'=>$books_id));
@@ -185,27 +207,49 @@ for($i=0; $i<$size; $i++)
                 $word_size = count($getall);    //取得数组单元个数
                 for($ws=0; $ws<$word_size; $ws++)
                 {
-                    $mysqlselect="select english,chinese,posd,US,UK from words where word_id=:word_id";
-                    $mysqlselect=$pdo->prepare($mysqlselect);
-                    $mysqlselect->execute(array(':word_id'=>$getall[$ws][0]));
-                    $getall[$ws]=NULL;
-                    $english=$mysqlselect->fetch();
-                    $posd=0;
-                    while ($english != NULL)
-                    {
-                        $tidy_english=[];
-                        for ($m=0;$m<count($english)/2;$m++)
-                        {
-                            array_push($tidy_english,$english[$m]);
-                        }
-        
-                        $getall[$ws][$posd]=$tidy_english;
+                    if ($word_ids[$i][1]=="1"){
+                        $mysqlselect="select english,chinese,posd,US,UK from self_defining_means where word_id=:word_id";
+                        $mysqlselect=$pdo->prepare($mysqlselect);
+                        $mysqlselect->execute(array(':word_id'=>$getall[$ws][0]));
+                        $getall[$ws]=NULL;
                         $english=$mysqlselect->fetch();
-                        $posd++;
-                        
+                        $posd=0;
+                        while ($english != NULL)
+                        {
+                            $tidy_english=[];
+                            for ($m=0;$m<count($english)/2;$m++)
+                            {
+                                array_push($tidy_english,$english[$m]);
+                            }
+            
+                            $getall[$ws][$posd]=$tidy_english;
+                            $english=$mysqlselect->fetch();
+                            $posd++;
+                            
+                        }
+                    }
+                    else{
+                        $mysqlselect="select english,chinese,posd,US,UK from words where word_id=:word_id";
+                        $mysqlselect=$pdo->prepare($mysqlselect);
+                        $mysqlselect->execute(array(':word_id'=>$getall[$ws][0]));
+                        $getall[$ws]=NULL;
+                        $english=$mysqlselect->fetch();
+                        $posd=0;
+                        while ($english != NULL)
+                        {
+                            $tidy_english=[];
+                            for ($m=0;$m<count($english)/2;$m++)
+                            {
+                                array_push($tidy_english,$english[$m]);
+                            }
+            
+                            $getall[$ws][$posd]=$tidy_english;
+                            $english=$mysqlselect->fetch();
+                            $posd++;
+                        }
                     }
                 }
-                array_push($data[$groups[$i][1]],$getall);
+                array_push($data[$groups[$i][1]],$review_array);
             }
             else{
                 // echo $groups[$i][1];
@@ -215,7 +259,7 @@ for($i=0; $i<$size; $i++)
             $review_array=[];
             if (($progress-50)>=0)
             {
-                $mysqlselect="select word_id from word_family where books_id=:books_id limit ".($progress-50).",50";
+                $mysqlselect="select word_id,self_defining from word_family where books_id=:books_id limit ".($progress-50).",50";
                 $mysqlselect=$pdo->prepare($mysqlselect);
                 $mysqlselect->execute(array(':books_id'=>$books_id));
                 $getall=$mysqlselect->fetchAll();
@@ -224,7 +268,7 @@ for($i=0; $i<$size; $i++)
         
             if (($progress-100)>=0)
             {
-                $mysqlselect="select word_id from word_family where books_id=:books_id limit ".($progress-100).",50";
+                $mysqlselect="select word_id,self_defining from word_family where books_id=:books_id limit ".($progress-100).",50";
                 $mysqlselect=$pdo->prepare($mysqlselect);
                 $mysqlselect->execute(array(':books_id'=>$books_id));
                 $getall=$mysqlselect->fetchAll();
@@ -232,7 +276,7 @@ for($i=0; $i<$size; $i++)
             }
             if (($progress-200)>=0)
             {
-                $mysqlselect="select word_id from word_family where books_id=:books_id limit ".($progress-200).",50";
+                $mysqlselect="select word_id,self_defining from word_family where books_id=:books_id limit ".($progress-200).",50";
                 $mysqlselect=$pdo->prepare($mysqlselect);
                 $mysqlselect->execute(array(':books_id'=>$books_id));
                 $getall=$mysqlselect->fetchAll();
@@ -240,7 +284,7 @@ for($i=0; $i<$size; $i++)
             }
             if (($progress-400)>=0)
             {
-                $mysqlselect="select word_id from word_family where books_id=:books_id limit ".($progress-400).",50";
+                $mysqlselect="select word_id,self_defining from word_family where books_id=:books_id limit ".($progress-400).",50";
                 $mysqlselect=$pdo->prepare($mysqlselect);
                 $mysqlselect->execute(array(':books_id'=>$books_id));
                 $getall=$mysqlselect->fetchAll();
@@ -248,34 +292,57 @@ for($i=0; $i<$size; $i++)
             }
             if (($progress-800)>=0)
             {
-                $mysqlselect="select word_id from word_family where books_id=:books_id limit ".($progress-800).",50";
+                $mysqlselect="select word_id,self_defining from word_family where books_id=:books_id limit ".($progress-800).",50";
                 $mysqlselect=$pdo->prepare($mysqlselect);
                 $mysqlselect->execute(array(':books_id'=>$books_id));
                 $getall=$mysqlselect->fetchAll();
                 $review_array=array_merge($getall,$review_array);
             }
+
             if (count($review_array) != 0){
                 $word_size = count($review_array);    //取得数组单元个数
                 for($ws=0; $ws<$word_size; $ws++)
                 {
-                    $mysqlselect="select english,chinese,posd,US,UK from words where word_id=:word_id";
-                    $mysqlselect=$pdo->prepare($mysqlselect);
-                    $mysqlselect->execute(array(':word_id'=>$review_array[$ws][0]));
-                    $review_array[$ws]=NULL;
-                    $english=$mysqlselect->fetch();
-                    $posd=0;
-                    while ($english != NULL)
+                    if ($review_array[$ws][1] == "1")
                     {
-                        $tidy_english=[];
-                        for ($m=0;$m<count($english)/2;$m++)
-                        {
-                            array_push($tidy_english,$english[$m]);
-                        }
-        
-                        $review_array[$ws][$posd]=$tidy_english;
+                        $mysqlselect="select english,chinese,posd,US,UK from self_defining_means where word_id=:word_id";
+                        $mysqlselect=$pdo->prepare($mysqlselect);
+                        $mysqlselect->execute(array(':word_id'=>$review_array[$ws][0]));
+                        $review_array[$ws]=NULL;
                         $english=$mysqlselect->fetch();
-                        $posd++;
-                        
+                        $posd=0;
+                        while ($english != NULL)
+                        {
+                            $tidy_english=[];
+                            for ($m=0;$m<count($english)/2;$m++)
+                            {
+                                array_push($tidy_english,$english[$m]);
+                            }
+            
+                            $review_array[$ws][$posd]=$tidy_english;
+                            $english=$mysqlselect->fetch();
+                            $posd++;
+                        }
+                    }
+                    else{
+                        $mysqlselect="select english,chinese,posd,US,UK from words where word_id=:word_id";
+                        $mysqlselect=$pdo->prepare($mysqlselect);
+                        $mysqlselect->execute(array(':word_id'=>$review_array[$ws][0]));
+                        $review_array[$ws]=NULL;
+                        $english=$mysqlselect->fetch();
+                        $posd=0;
+                        while ($english != NULL)
+                        {
+                            $tidy_english=[];
+                            for ($m=0;$m<count($english)/2;$m++)
+                            {
+                                array_push($tidy_english,$english[$m]);
+                            }
+            
+                            $review_array[$ws][$posd]=$tidy_english;
+                            $english=$mysqlselect->fetch();
+                            $posd++;
+                        }
                     }
                 }
                 array_push($data[$groups[$i][1]],$review_array);
